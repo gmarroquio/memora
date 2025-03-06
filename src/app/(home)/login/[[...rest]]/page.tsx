286bf1ed594a592ba035ea9b737bcdf47366ffe3
Guilherme Marroquio <guilhermemarroquio@gmail.com>
2025-03-06 06:37:07 -0300

    login page

--- src/app/(home)/login/[[...rest]]/page.tsx
+++ src/app/(home)/login/[[...rest]]/page.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { SignIn, SignUp } from "@clerk/nextjs";
+import text from "@/constants/texts.json";
 
 export default function LoginPage() {
   const [activeTab, setActiveTab] = useState<"login" | "signup">("login");
@@ -12,21 +13,29 @@ export default function LoginPage() {
       <main className="flex-1 flex items-center justify-center">
         <div className="w-full max-w-md p-6">
           <h1 className="text-2xl font-bold text-center mb-6">
-            Welcome to Memora
+            {text.pt.login_page.title}
           </h1>
           <Tabs
             value={activeTab}
             onValueChange={(value) => setActiveTab(value as "login" | "signup")}
           >
             <TabsList className="grid w-full grid-cols-2">
-              <TabsTrigger value="login">Login</TabsTrigger>
-              <TabsTrigger value="signup">Sign Up</TabsTrigger>
+              <TabsTrigger value="login">
+                {text.pt.login_page.tabs.login}
+              </TabsTrigger>
+              <TabsTrigger value="signup">
+                {text.pt.login_page.tabs.signup}
+              </TabsTrigger>
             </TabsList>
             <TabsContent value="login">
-              <SignIn fallbackRedirectUrl="/dashboard" />
+              <SignIn
+                fallbackRedirectUrl={text.pt.login_page.fallback_redirect_url}
+              />
             </TabsContent>
             <TabsContent value="signup">
-              <SignUp fallbackRedirectUrl="/dashboard" />
+              <SignUp
+                fallbackRedirectUrl={text.pt.login_page.fallback_redirect_url}
+              />
             </TabsContent>
           </Tabs>
         </div>
