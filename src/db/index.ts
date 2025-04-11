b2fb62c211c11446282cfd54226ddaf05fb964ac
Guilherme Marroquio <guilhermemarroquio@gmail.com>
2025-04-11 00:52:15 -0300

    second try

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,3 +1,3 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 
-export const db = drizzle({ connection: { url: process.env.DATABASE_URL! } });
+export const db = drizzle(process.env.DATABASE_URL!);
