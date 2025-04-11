0a4511a2c3795893bd497ebd187e6d5ecd630480
Guilherme Marroquio <guilhermemarroquio@gmail.com>
2025-04-11 01:01:33 -0300

    official supabase

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,5 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import pg from "postgres";
 
-const client = pg(process.env.DATABASE_URL!);
+const client = pg(process.env.DATABASE_URL!, { prepare: false });
 export const db = drizzle({ client });
