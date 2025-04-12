89410d60a590dc054700eef006ed7c30a250f98e
Guilherme Marroquio <guilhermemarroquio@gmail.com>
2025-04-12 07:52:04 -0300

    check if album code exists

--- src/app/api/code/route.ts
+++ src/app/api/code/route.ts
@@ -9,8 +9,28 @@ export async function POST(req: NextRequest) {
   if (!userId)
     return NextResponse.json({ message: "User unauthorized" }, { status: 401 });
   const body = await req.json();
-  const code = Math.random().toString(36).substring(2, 8).toUpperCase();
+  let code = Math.random().toString(36).substring(2, 8).toUpperCase();
   const expires = addDays(new Date(), body.expirationDays);
+
+  let [codeExists] = await db
+    .select()
+    .from(codesTable)
+    .where(eq(codesTable.code, code));
+  if (codeExists) {
+    code = Math.random().toString(36).substring(2, 8).toUpperCase();
+  }
+
+  [codeExists] = await db
+    .select()
+    .from(codesTable)
+    .where(eq(codesTable.code, code));
+
+  if (codeExists) {
+    return NextResponse.json(
+      { message: "Code creation error. Try again later!" },
+      { status: 500 }
+    );
+  }
 
   const [album] = await db
     .select()
