e01281cde9a1d98c567b7ddc1d2c56c20e25809a
Guilherme Marroquio <guilhermemarroquio@gmail.com>
2025-04-22 10:37:30 -0300

    add pegeleave event

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -29,7 +29,6 @@ function PostHogPageView() {
   const searchParams = useSearchParams();
   const posthog = usePostHog();
 
-  // Track pageviews
   useEffect(() => {
     if (pathname && posthog) {
       let url = window.origin + pathname;
@@ -39,14 +38,22 @@ function PostHogPageView() {
 
       posthog.capture("$pageview", { $current_url: url });
     }
+
+    return () => {
+      if (pathname && posthog) {
+        let url = window.origin + pathname;
+        if (searchParams.toString()) {
+          url = url + "?" + searchParams.toString();
+        }
+
+        posthog.capture("$pageleave", { $current_url: url });
+      }
+    };
   }, [pathname, searchParams, posthog]);
 
   return null;
 }
 
-// Wrap PostHogPageView in Suspense to avoid the useSearchParams usage above
-// from de-opting the whole app into client-side rendering
-// See: https://nextjs.org/docs/messages/deopted-into-client-rendering
 function SuspendedPostHogPageView() {
   return (
     <Suspense fallback={null}>
